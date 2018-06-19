fce16028d7c4e4997c22561d7ca87c09192eccae
Pritty Skaria <prittyskaria@gmail.com>
2018-06-19 11:03:41 +0100

    modified function such that once used passwords cannot be used again, passwords are the mapping key to address and balance, finally transfer made to the address that the password matches

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -4,48 +4,55 @@ contract Remittance {
 
     address  public owner;
    
-    struct Details{
+    struct DetailsStruct{
+        address to;
         uint balance;
-        bytes32 hashPassword;
+        bool isUsed;
     }
     
-    mapping (address => Details) public Remittee;
+    mapping (bytes32 => DetailsStruct) public Remittee;
     
     event LogWithdraw(address Receiver, uint Amount);
     event LogsendRemittance(address Receiver, bytes32 hashPassword, uint Amount);
-
+    
+    modifier onlyBy(address byWhom)
+    {
+        require(msg.sender == byWhom);
+         _;
+    }
+  
     function Remittance () public payable{
         owner = msg.sender;
     }
     
-    function sendRemittance (address to, bytes32 hashPassword) public payable returns(bool){
-       require(msg.sender == owner);
-       Remittee[to].balance += msg.value;
-       Remittee[to].hashPassword = hashPassword;
+    function sendRemittance (address to, bytes32 hashPassword) public payable  onlyBy(owner) returns(bool){
+       require(!Remittee[hashPassword].isUsed);
+       Remittee[hashPassword].to = to;
+       Remittee[hashPassword].balance += msg.value;
+       Remittee[hashPassword].isUsed = true;
        LogsendRemittance(to , hashPassword, msg.value);
        return true;
     }
     
     function withdraw(address to, bytes32 password) public{
-        require(Remittee[to].hashPassword == hashHelper(password));
-        uint amount = Remittee[to].balance;
+        bytes32 hashPassword = hashHelper(password);
+        require(Remittee[hashPassword].to == to);
+        uint amount = Remittee[hashPassword].balance;
         require (amount > 0);
-        delete Remittee[to];
-        LogWithdraw(to, amount);
-        to.transfer(amount);
+        Remittee[hashPassword].balance = 0;
+        LogWithdraw(Remittee[hashPassword].to, amount);
+        Remittee[hashPassword].to.transfer(amount);
     }
     
     function hashHelper (bytes32 pw) public pure returns(bytes32 hash){
        return keccak256(pw);
     }
 
-    
     function () public payable{ 
         revert();
     }
     
-    function kill() public{
-        require(msg.sender == owner);
+    function kill() public onlyBy(owner){
         selfdestruct(owner);
     }
     
